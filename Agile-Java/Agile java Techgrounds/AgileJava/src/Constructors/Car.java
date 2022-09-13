8a96839d01b0fc836e40dbd74acf887410112e02
dwahed <djaihoonwahed>
2022-09-13 08:18:27 +0200

    toevoegen van regresie test

---
+++ Agile-Java/Agile java Techgrounds/AgileJava/src/Constructors/Car.java
@@ -0,0 +1,18 @@
+package Constructors;
+public class Car {
+
+    String color;
+
+    public Car(String color) {
+        this.color = color;
+
+
+
+    }
+}
+
+
+
+
+
+
---
+++ Agile-Java/Agile java Techgrounds/AgileJava/src/Constructors/Constructors.java
@@ -0,0 +1,12 @@
+package Constructors;
+
+public class Constructors {
+    public static void main(String[] args) {
+
+        Car myCar = new Car("Groen");
+        System.out.println(myCar.color);
+
+
+
+    }
+}
